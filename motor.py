011e70102d4d1288fc7537138d4c8685cc7adb47
Voncruyff <151720103+Voncruyff@users.noreply.github.com>
2024-10-16 09:57:47 +0700

    Add files via upload

---
+++ motor.py
@@ -0,0 +1,8 @@
+class motor:
+    barang = ""
+    harga = 0
+motormainan = motor()
+motormainan.harga = "Rp.500.000"
+motormainan.barang = "Motor Mainan"
+
+print(f"Barang: {motormainan.barang}, Harga: {motormainan.harga}") 
